5e2226d54905fec7c920a76a29987ef6ce67072d
Jean-Charles CANO <jeancharles.cano@bastide-medical.fr>
2024-03-06 17:01:05 +0100

    label

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-function App() {
+import sheep from "./assets/img/sheep.png";
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div className="h-screen w-screen bg-midnight-blue">
+      <div className="h-1/3"></div>
+      <div className="w-full h-2/3 flex justify-center items-center">
+        <div
+          id="label"
+          className="h-[5cm] w-[10cm] bg-[#F0F8FF] flex items-center justify-between overflow-hidden p-2 rounded"
         >
-          Learn React
-        </a>
-      </header>
+          <img src={sheep} className="object-fill h-4/6" />
+          <div className="h-4/5 flex flex-col justify-between items-end">
+            <h1 id="title" className="text-2xl font-semibold">
+              Peau de mouton
+            </h1>
+            <h2 className="text-xl font-bold">75 €</h2>
+          </div>
+        </div>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: { 
+      colors: {
+        "midnight-blue": "#101720",
+        "midnight": "#121063"
+      }
+     },
+    
+  },
+  plugins: [],
+}
+
